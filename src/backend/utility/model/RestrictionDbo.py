cfb7c0371f0fa9cd6f65b2cf24d1be0576518077
h0dduck <82387475+h0dduck@users.noreply.github.com>
2025-12-04 19:47:49 +0900

    delete all files

--- src/backend/utility/model/RestrictionDbo.py
+++
@@ -1,7 +0,0 @@
-class RestrictionDbo:
-    def __init__(self, id=None, name=None):
-        self.id = id
-        self.name = name
-
-    def __repr__(self):
-        return f"RestrictionListDbo(id={self.id}, name='{self.name}'"
--- src/smarterspeaker/Users/guest_user.py
+++
@@ -1,5 +0,0 @@
-from user import User
-
-class GuestUser(User):
-    def __init__(self, voice=None):
-        super().__init__(user_id="guest", name="Guest", voice=voice, role="guest")
